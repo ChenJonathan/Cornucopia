ea21102521d605d162550287c0d165ae2d056cc3
Jonathan Chen <jonathanchen@gatech.edu>
2016-09-24 16:01:43 -0400

    Changed database path

--- app_server/controllers/recipes.js
+++ app_server/controllers/recipes.js
@@ -1,44 +1,20 @@
 var request = require('request');
 
 module.exports.recipeInfo = function(req, res) {
-    request.get('http://localhost:3000/api/recipe/' + req.params.recipeId, function(err, response) {
+    request.get('http://chenjonathan-cornucopia.herokuapp.com/api/recipe/' + req.params.recipeId, function(err, response) {
         if (err) {
             next(err);
         }
         var recipe = response['body'];
-        console.log(recipe);
         res.render('recipes/info', {
+            title: recipe
         });
     });
 };
 
 
-//FIX ARRAY ISSUE
 module.exports.recipeCreate = function(req, res) {
-    var options = {
-        method: 'POST',
-        uri: 'http://localhost:3000/api/recipe',
-        form: {
-            'name': 'food',
-            'author': 'user',
-            'instructions': ['cook', 'bake'],
-            'rating': 5,
-            'ingredients': ['flour', 'salt', 'sugar']
-        }
-    };
-    request(options, function(err, res, body) {
-        if (err) {
-            console.log(err);
-            next(err);
-        } else {
-            console.log(res);
-        }
-    });
     res.render('recipes/create', {
-        name: 'food',
-        author: 'user',
-        instructions: ['cook', 'bake'],
-        rating: 5,
-        ingredients: ['flour', 'salt', 'sugar']
+        title: "Test"
     });
 };
