77572f1c6594f5510cff09f2a5f74c15f0224676
achiang31 <alan.chiang@live.com>
2016-09-24 13:21:29 -0400

    login authentication

--- app_server/controllers/other.js
+++ app_server/controllers/other.js
@@ -7,6 +7,15 @@ module.exports.home = function(req, res) {
 };
 
 module.exports.login = function(req, res) {
+    router.get('http://localhost:3000/user/' + req.params.userId + '/'req.params.password, function (err, response) {
+        if (err) {
+            console.log(err);
+            next(err);
+        }
+        if (!(assert.equal(response.body.user, req.params.userId) && assert.equal(response.body.password, req.params.password))) {
+            alert("Incorrect combination of username and password");
+        }
+    }
     res.render('other\\login', {
         title: 'Login',
         username: 'Username:',
@@ -15,6 +24,15 @@ module.exports.login = function(req, res) {
 };
 
 module.exports.register = function(req, res) {
+    router.get('http://localhost:3000/user/' + req.params.userId + '/'req.params.password + '/'req.params.vpassword, function (err, response) {
+        if (err) {
+            console.log(err);
+            next(err);
+        }
+        if (!(assert.equal(req.params.vpassword, req.params.password))) {
+            alert("Passwords do not match");
+        }
+    }
     res.render('other\\registration', {
         title: 'New User Registration',
         newUsername: 'New Username:',
