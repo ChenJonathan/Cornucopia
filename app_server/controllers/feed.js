aa3fb485280f453fb9367b8b08ca30dc1a5f0f5a
achiang31 <alan.chiang@live.com>
2016-09-24 12:30:39 -0400

    feed

--- app_server/controllers/feed.js
+++ app_server/controllers/feed.js
@@ -1,6 +1,11 @@
 /* GET 'location list' page. */
 module.exports.feedList = function(req, res) {
+
+    var feed;
+    request.get()
+
     res.render('feed\\feed', {
         title: 'Test list',
     });
+    recipe:
 };
