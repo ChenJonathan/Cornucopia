eb1dc128abdfff4a6db966fe2ff265ca934d45d6
achiang31 <alan.chiang@live.com>
2016-09-24 08:11:17 -0400

    pulling heroku

--- app_server/controllers/recipes.js
+++ app_server/controllers/recipes.js
@@ -1,12 +1,12 @@
 /* GET 'location list' page. */
 module.exports.recipeInfo = function(req, res) {
-    res.render('recipes/info', {
+    res.render('/recipe/:recipeId', {
         title: 'Test ' + req.params.recipeId,
     });
 };
 
 module.exports.recipeCreate = function(req, res) {
-    res.render('recipes/create', {
+    res.render('/recipes', {
         title: 'Test new',
     });
 };
