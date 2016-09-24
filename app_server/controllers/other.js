6a1174f7f38f263d595bc2f2a42f001256a0d150
achiang31 <alan.chiang@live.com>
2016-09-24 02:02:45 -0400

    Login / register

--- app_server/controllers/other.js
+++ app_server/controllers/other.js
@@ -1,6 +1,25 @@
 /* GET 'about' page. */
 module.exports.home = function(req, res) {
     res.render('index', {
-        title: 'Test home',
+        title: 'Homepage',
+        input1: 'Login Button',
+        input2: 'Password Button',
+    });
+};
+
+module.exports.login = function(req, res) {
+    res.render('index', {
+        loginTitle: 'Login',
+        username: 'Username:',
+        password: 'Password:',
+    });
+};
+
+module.exports.register = function(req, res) {
+    res.render('index', {
+        registerTitle: 'New User Registration',
+        newUsername: 'New Username:',
+        newPassword: 'New Password:',
+        verifyPassword: 'Verify Password',
     });
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -6,6 +6,8 @@ var controlOther = require('../controllers/other');
 
 /* Recipe pages */
 router.get('/', controlOther.home);
+router.get('/login', controlOther.login);
+router.get('/register', controlOther.register);
 router.get('/recipes', controlRecipes.recipeList);
 
 module.exports = router;
