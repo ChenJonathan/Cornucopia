6b2ec906510c604534dc255eb9da76f54096b29f
achiang31 <alan.chiang@live.com>
2016-09-24 16:36:52 -0400

    fixed directory

--- app.js
+++ app.js
@@ -59,42 +59,42 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-//add data test
+/*add data test
 
-// describe('Post recipe', function() {
-//     it('should return success after adding new recipe', function(done) {
-//         var request = require('supertest');
-//         var recipe = {
-//             "ingredients" : [
-//                 {
-//                     "flour" : "2 cups",
-//                     "sugar" : "1 cup",
-//                     "butter" : "1 stick",
-//                     "eggs" : "four"
-//                 },
-//             ],
-//             "name" : "Manhattan Cake",
-//             "author" : "John",
-//             "instructions" : [
-//                 {
-//                     "prep" : "stuff",
-//                     "cooking" : "cook",
-//                     "serve" : "lightly incinerated"
-//                 },
-//             ],
-//             "name" : "Chen",
-//             "rating" : 1
-//         };
-//
-//         request(url)
-//             .post('/api/recipe')
-//
-//             .end(function(err, res) {
-//                 if (err) {
-//                     throw err;
-//                 }
-//                 res.should.have.property(‘status’, 201);
-//                 done();
-//             });
-//     });
-// });
+ describe('Post recipe', function() {
+     it('should return success after adding new recipe', function(done) {
+         var request = require('supertest');
+         var recipe = {
+             "ingredients" : [
+                 {
+                     "flour" : "2 cups",
+                     "sugar" : "1 cup",
+                     "butter" : "1 stick",
+                     "eggs" : "four"
+                 },
+             ],
+             "name" : "Manhattan Cake",
+             "author" : "John",
+             "instructions" : [
+                 {
+                     "prep" : "stuff",
+                     "cooking" : "cook",
+                     "serve" : "lightly incinerated"
+                  },
+             ],
+             "name" : "Chen",
+             "rating" : 1
+         };
+
+         request(url)
+            .post('/api/recipe')
+
+             .end(function(err, res) {
+                 if (err) {
+                     throw err;
+                 }
+                 res.should.have.property(‘status’, 201);
+                 done();
+             });
+     });
+ }); */
