6965ce2b3e94256ff590e4a7d86d5d4e8e9d5b1c
kevin-tian <kevin_tian@outlook.com>
2016-09-24 02:39:39 -0400

    Added bootstrap to layout and navbar

--- app_server/controllers/recipes.js
+++ app_server/controllers/recipes.js
@@ -1,6 +1,6 @@
 /* GET 'location list' page. */
 module.exports.recipeList = function(req, res) {
-    res.render('index', {
-        title: 'Test list',
+    res.render('index', { //this is the name of the jade file we're pushing stuff too
+        title: 'Test list', //this is the list of json that is being sent to the page
     });
 };
