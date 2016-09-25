76b4b15c901a7b6daf53f9c28fbde1fc5bb515d7
achiang31 <alan.chiang@live.com>
2016-09-25 01:37:50 -0400

    groceriesDisplay

--- app_server/controllers/profile.js
+++ app_server/controllers/profile.js
@@ -35,19 +35,37 @@ module.exports.profileRecipes = function(req, res, next) {
     });
 };
 
+/*var globalData;
+
+//get data from ajax call and store it in a global variable
+function ajax1() {
+    var tmp = null;
+    $.ajax({
+        async: false,
+        type: "GET",
+        url: 'http://chenjonathan-cornucopia.herokuapp.com/api/user/',
+        data: {appName: $.query.get("appName")},
+        success: function (data) {
+            globalData = data;
+            tmp = data;
+        }
+    });
+
+    return tmp;
+}*/
+
 module.exports.profileGroceries = function(req, res, next) {
     request.get('http://chenjonathan-cornucopia.herokuapp.com/api/user/' + req.params.userId + '/highlighted', function(err, highlightedRecipes) {
         if (err) {
             next(err);
         }
-        $.when(ajax1()).done(function () {
+        //$.when(ajax1()).done(function () {
             request.get('http://chenjonathan-cornucopia.herokuapp.com/api/recipe/:recipeId/ingredients' + req.params.recipeId + '/ingredients', function(error, components) {
-
+                res.render('profile/groceries', {
+                    selected : JSON.parse(highlightedRecipes.body),
+                    ingredients : JSON.parse(components.body)
             });
         });
     });
-    res.render('profile/groceries', {
-        selected : JSON.parse(savedRecipes.body),
-        ingredients : JSON.parse(components.body)
-    });
+
 };
