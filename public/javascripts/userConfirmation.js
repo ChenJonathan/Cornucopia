d5f9c25e29fe888264c55f0eef6fef081a177c64
kevin-tian <kevin_tian@outlook.com>
2016-09-25 02:13:12 -0400

    Added user authentication on log-in

--- public/javascripts/userConfirmation.js
+++ public/javascripts/userConfirmation.js
@@ -1,14 +1,26 @@
 /**
  * Created by kevin on 9/25/2016.
  */
-$(function() {
-    $.ajax({
-        type: 'GET',
-        url: 'http://chenjonathan-cornucopia.herokuapp.com/api/user',
-        success: function(data) {
-            console.log(data);
-        }
-
-    });
-
-})
+$(function () {
+    $("#login").click(function () {
+        $.ajax({
+            type: 'GET',
+            url: 'http://chenjonathan-cornucopia.herokuapp.com/api/user',
+            success: function (data) {
+                for (var i = 0; i < data.length; i++) {
+                    if ($('#user').val() === data[i].user) {
+                        if ($('#pass').val() === data[i].password) {
+                            console.log(data[i].password);
+                            var newURL = 'http://chenjonathan-cornucopia.herokuapp.com/users/' + data[i]._id + '/recipes';
+                            document.location.href = newURL;
+                        } else {
+                            window.alert("Wrong Username/Password Combination!");
+                        }
+                    } else {
+                        window.alert("Wrong Username/Password Combination!");
+                    }
+                }
+            }
+        });
+    }
+)});
