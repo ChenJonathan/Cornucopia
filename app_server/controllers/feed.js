9b1abd8b92469ddf0a911d21016c07dc6f9aa0e0
msternberg <matt.sternberg@aol.com>
2016-09-24 20:27:31 -0400

    Connected the view and server-side json data

--- app_server/controllers/feed.js
+++ app_server/controllers/feed.js
@@ -2,9 +2,10 @@ var request = require('request');
 
 module.exports.feedList = function(req, res) {
     request.get("http://chenjonathan-cornucopia.herokuapp.com/api/recipe", function(err, response) {
+        console.log(JSON.parse(response.body));
         res.render('feed/feed', {
             title: "Test",
-            recipes: response.body
+            recipes: JSON.parse(response.body)
         });
     });
 };
